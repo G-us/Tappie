ee5271ced7b67e4498f59daefbac8a52730d239e
henryp <henrygparsons@gmail.com>
2023-04-28 20:37:55 +0200

    well, it works now so yay 🎊

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,49 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
 #include <BleKeyboard.h>
 #include <Arduino.h>
+#include <OneButton.h>
 
 BleKeyboard bleKeyboard;
+#define buttonPin 15 // the number of the pushbutton pin
+bool buttonState = false; // variable to hold the button state
+bool lastButtonState = false; // variable to hold the last button state
+OneButton btn = OneButton(
+  buttonPin,  // Input pin for the button
+  true,        // Button is active LOW
+  true         // Enable internal pull-up resistor
+);
 
-void setup() {
+void PlayPause()
+{
+  Serial.println("Sending Play/Pause media key...");
+  bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+}
+
+void NextTrack()
+{
+  Serial.println("Sending Next Track media key...");
+  bleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
+}
+
+void PreviousTrack()
+{
+  Serial.println("Sending Previous Track media key...");
+  bleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
+}
+
+void setup()
+{
   Serial.begin(115200);
   Serial.println("Starting BLE work!");
   bleKeyboard.begin();
+  btn.attachClick(PlayPause);
+  btn.attachDoubleClick(NextTrack);
+  btn.attachLongPressStart(PreviousTrack);
 }
 
-void loop() {
-  if(bleKeyboard.isConnected()) {
-    Serial.println("Sending 'Hello world'...");
-    bleKeyboard.print("Hello world");
-
-    delay(1000);
-
-    Serial.println("Sending Enter key...");
-    bleKeyboard.write(KEY_RETURN);
-
-    delay(1000);
-
-    Serial.println("Sending Play/Pause media key...");
-    bleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
-
-    delay(1000);
-
-   //
-   // Below is an example of pressing multiple keyboard modifiers 
-   // which by default is commented out.
-    /*
-    Serial.println("Sending Ctrl+Alt+Delete...");
-    bleKeyboard.press(KEY_LEFT_CTRL);
-    bleKeyboard.press(KEY_LEFT_ALT);
-    bleKeyboard.press(KEY_DELETE);
-    delay(100);
-    bleKeyboard.releaseAll();
-    */
+void loop()
+{
+  if (bleKeyboard.isConnected())
+  {
+    btn.tick(); // check the status of the button
   }
-
-  Serial.println("Waiting 5 seconds...");
-  delay(5000);
 }
